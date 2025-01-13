86e2650f1d4897a1919ad60ed57b0f1025e8784c
Ricky <77863853+RickyYCheng@users.noreply.github.com>
2025-01-13 13:14:58 +0800

    Update utils.fsx

--- utils.fsx
+++ utils.fsx
@@ -1,12 +1,22 @@
 [<AutoOpen>]
 module Utils
+
+#nowarn 9
 #nowarn 42
+
+open System
+open FSharp.NativeInterop
+
+let inline stackalloc<'a when 'a: unmanaged> (length: int): Span<'a> =
+    let p = NativePtr.stackalloc<'a> length |> NativePtr.toVoidPtr
+    Span<'a>(p, length)
+
 let inline bitcast<'a, 'b> (x:'a) = (# "" x : 'b #)
 
 /// target netstandard2.0
 type [<AbstractClass; Sealed>] Random() = 
     static let _global = System.Random()
-    [<DefaultValue; System.ThreadStatic>] static val mutable private _local : System.Random
+    [<DefaultValue; ThreadStatic>] static val mutable private _local : System.Random
 
     static member private instance() = 
         let mutable rnd = Random._local
